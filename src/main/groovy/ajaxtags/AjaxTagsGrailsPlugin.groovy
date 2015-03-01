e8b8cffbcc8f5a6e068b22f306474556b22e69e9
Jeff Scott Brown <jbrown@pivotal.io>
2015-03-01 10:57:30 -0600

    update plugin metadata

--- src/main/groovy/ajaxtags/AjaxTagsGrailsPlugin.groovy
+++ src/main/groovy/ajaxtags/AjaxTagsGrailsPlugin.groovy
@@ -1,44 +1,35 @@
 package ajaxtags
 
-import grails.plugins.*
+import grails.plugins.Plugin
 
 class AjaxTagsGrailsPlugin extends Plugin {
 
    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "3.0.0.M2 > *"
+
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
-    def title = "Ajax Tags" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "AJAX Tags"
+    def author = "Jeff Brown"
+    def authorEmail = "jeff@jeffandbetsy.net"
     def description = '''\
-Brief summary/description of the plugin.
+The ajax-tags plugin provides AJAX related GSP tags.
 '''
     def profiles = ['web']
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/ajax-tags"
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def license = "APACHE"
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GitHub Issues", url: "http://github.com/grails3-plugins/ajax-tags/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/grails3-plugins/ajax-tags/" ]
 
     Closure doWithSpring() { {->
             // TODO Implement runtime spring config (optional)
